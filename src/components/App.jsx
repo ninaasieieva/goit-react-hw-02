154df388e43845e28ed00d8f00658358885fa4f3
ninaasieieva <160421199+ninaasieieva@users.noreply.github.com>
2024-08-16 20:02:35 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,13 +28,7 @@ function App() {
     }
   };
 
-  return (
-    <>
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+
 
       <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
 
